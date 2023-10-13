5e0ae0ead6ea4d341fb23fe67ad9055587985147
Arthur Reis <reisarthur04@gmail.com>
2023-10-12 22:13:40 -0300

    segundo commit - mais telas e hook form

--- src/telasAdv/LoginAdv.js
+++ src/telasAdv/LoginAdv.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import { View, TextInput, Text, TouchableWithoutFeedback, TouchableOpacity, Image, ScrollView, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import {useForm, Controller} from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import { styles } from '../Styles.js';
 
 export default function LoginAdv({ navigation }) {
-  const [email, setEmail] = useState(null);
-  const [senha, setSenha] = useState(null);
-
-  const logar = () => {
+  const logar = (data) => {
     navigation.reset({
       index: 0,
       routes: [{ name: 'TabRoutesAdv' }]
     })
+    console.log(data);
   }
+
+  const schema = yup.object({
+    email: yup.string().required('Informe seu e-mail').email('E-mail inválido'),
+    senha: yup.string().required('Digite uma senha').min(8, 'Pelo menos 8 caracteres')
+  })
+
+  const {control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
 
   return (
     <View style={styles.container}>
@@ -24,34 +34,61 @@ export default function LoginAdv({ navigation }) {
       </View>
       <Text style={styles.text4}>ADVOGADO</Text>
       <TouchableWithoutFeedback>
-        <ScrollView style={{ paddingTop: 20, width: '90%' }} showsVerticalScrollIndicator={false}>
+        <ScrollView style={{ paddingTop: 35, width: '90%' }} showsVerticalScrollIndicator={false}>
           <View style={styles.inputContainer}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'center', }}>
-              <FontAwesome name="envelope" size={20} color="#1E5A97" style={{ marginRight: 10 }}/>
-              <TextInput
-                placeholder="E-mail"
-                keyboardType='email-address'
-                autoCapitalize='none'
-                autoComplete='email'
-                value={email}
-                onChangeText={value => setEmail(value)}
-                style={styles.inputLogin}
-              />
-            </View>
             <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'center', }}>
-              <FontAwesome name="lock" size={25} color="#1E5A97" style={{ marginRight: 10,  }}/>
-              <TextInput
-                placeholder="Senha"
-                value={senha}
-                onChangeText={value => setSenha(value)}
-                style={styles.inputLogin}
-                secureTextEntry
+              <FontAwesome name="envelope" size={20} color="#1E5A97" style={[{ marginRight: 10 }, { marginBottom: errors.email ? 3.5 : 16 }]}/>
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: {onChange, onBlur, value}}) => (
+                  <TextInput
+                  style={[
+                    styles.inputLogin, {
+                      borderWidth: errors.email ? 1.5 : 1,
+                      borderColor: errors.email ? '#f23535' : '#1E5A97',
+                      marginBottom: errors.email ? 5 : 16
+                    }]}
+                  placeholder="E-mail"
+                  keyboardType='email-address'
+                  autoCapitalize='none'
+                  autoComplete='email'
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur} //quando o textinput é tocado
+                  />
+                )}
               />
             </View>
+            {errors.email && <Text style={styles.inputLoginError}>{errors.email?.message}</Text>}
+
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'center', }}>
+              <FontAwesome name="lock" size={25} color="#1E5A97" style={[{ marginRight: 10 }, { marginBottom: errors.email ? 3.5 : 16 }]}/>
+              <Controller
+                control={control}
+                name="senha"
+                render={({ field: {onChange, onBlur, value}}) => (
+                  <TextInput
+                  style={[
+                    styles.inputLogin, {
+                      borderWidth: errors.senha ? 1.5 : 1,
+                      borderColor: errors.senha ? '#f23535' : '#1E5A97',
+                      marginBottom: errors.senha ? 5 : 16
+                    }]}
+                  placeholder="Senha"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  secureTextEntry={true}
+                  />
+                )}
+              />
+            </View>
+            {errors.senha && <Text style={styles.inputLoginError}>{errors.senha?.message}</Text>}
           </View>
 
           <View style={styles.buttonContainer}>
-            <TouchableOpacity onPress={logar} style={styles.button}>
+            <TouchableOpacity onPress={handleSubmit(logar)} style={styles.button}>
               <Text style={styles.loginButtonText}>ENTRAR</Text>
             </TouchableOpacity>
           </View>
